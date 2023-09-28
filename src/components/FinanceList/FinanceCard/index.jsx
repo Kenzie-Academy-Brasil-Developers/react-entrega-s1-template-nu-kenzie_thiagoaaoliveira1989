c0c7e74c01cfd346c83f2b678d60c9033a6257e5
Thiago Araujo de Aguiar Oliveira <thiagoaaoliveira1989@gmail.com>
2023-09-28 16:07:36 -0300

    feat: update conditional component FinanceCard

--- src/components/FinanceList/FinanceCard/index.jsx
+++ src/components/FinanceList/FinanceCard/index.jsx
@@ -2,27 +2,19 @@ import styles from "./style.module.scss";
 
 export const FinanceCard = ({ removeFinance, id, description, price, type }) => {
 
-    return type === "saida" ? (
-        <li key={id} className={styles.disable}>
-            <div>
-                <h3 className="title three"> {description} </h3>
-                <p className="title four">{type}</p>
-            </div>
-            <div className={styles.divValue}>
-                <p className="title four"> {price.toLocaleString("pt-br", { style: "currency", currency: "BRL" })} </p>
-                <button onClick={() => removeFinance(id, description)} className={styles.btnList}>Excluir</button>
-            </div>
-        </li>
-    ) : (
-        <li key={id} className={styles.flex}>
-            <div>
-                <h3 className="title three"> {description} </h3>
-                <p className="title four">{type}</p>
-            </div>
-            <div className={styles.divValue}>
-                <p className="title four"> {price.toLocaleString("pt-br", { style: "currency", currency: "BRL" })} </p>
-                <button onClick={() => removeFinance(id, description)} className={styles.btnList}>Excluir</button>
-            </div>
-        </li>
-    );
+    return (
+        <>              
+            <li key={id} className={type === "entrada" ? styles.flex : styles.disable}>
+                <div>
+                    <h3 className="title three"> {description} </h3>
+                    <p className="title four">{type}</p>
+                </div>
+                <div className={styles.divValue}>
+                    <p className="title four"> {price.toLocaleString("pt-br", { style: "currency", currency: "BRL" })} </p>
+                    <button onClick={() => removeFinance(id, description)} className={styles.btnList}>Excluir</button>
+                </div>
+            </li>
+
+        </>
+    )
 }
